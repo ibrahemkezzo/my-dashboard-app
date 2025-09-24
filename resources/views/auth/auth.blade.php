35a7ea002440ce24537aa5674896b8521f285818
ibrahem kezzo <ibrahemkezzo.w@gmail.com>
2025-09-24 18:56:50 +0300

    hot fix for load css file auth

--- resources/views/auth/auth.blade.php
+++ resources/views/auth/auth.blade.php
@@ -233,5 +233,5 @@
 
 @push('scripts')
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js"></script>
-    <script src="{{ asset('frontend/assets/js/auth.js') }}"></script>
+    <script src="{{ asset('frontend/assets/js/auth.js?v=1.0.1') }}"></script>
 @endpush
